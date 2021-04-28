00becb23f4f7c6c5d04e3032d07424868288a256
ismabarroso <ismaelbarroso2001@ginebro.cat>
2021-04-28 18:41:13 +0200

    ExtVarLocal

---
+++ BarrosoIsmaelApp.java
@@ -0,0 +1,24 @@
+public class BarrosoIsmaelApp {
+
+	public static void main(String[] args) {
+
+		//Declaramos las variables
+		int num1=10;
+		int num2=5;
+
+		/*Realizamos las operaciones.
+		 * Tambien lo podemos guardar el resultado en variables. */
+
+		String mesage1 = "El resultado de la suma es ";
+		System.out.println(mesage1+(num1+num2));
+
+		String msg2 = "El resultado de la resta es ";
+		System.out.println(msg2+(num1-num2));
+
+		String msg3 = "El resultado de la multiplicación es ";
+		System.out.println(msg3+(num1*num2));
+
+		String msg4 = "El resultado de la división es ";
+		System.out.println(msg4+(num1/num2));
+	}
+}
